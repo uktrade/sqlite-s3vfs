a872a6f782e4d1d65fffcfeddba62384aa762c3a
Michal Charemza <michal@charemza.name>
2021-09-30 20:21:55 +0100

    tests: ready to test more journal modes

--- test_sqlite_s3vfs.py
+++ test_sqlite_s3vfs.py
@@ -7,6 +7,7 @@ import pytest
 from sqlite_s3vfs import S3VFS
 
 SIZES = [512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]
+JOURNAL_MODES = ['DELETE']
 
 @pytest.fixture
 def bucket():
@@ -18,6 +19,7 @@ def bucket():
     s3 = session.resource('s3',
         endpoint_url='http://localhost:9000/'
     )
+
     bucket = s3.create_bucket(Bucket=f's3vfs-{str(uuid.uuid4())}')
     yield bucket
     bucket.objects.all().delete()
@@ -30,13 +32,21 @@ def bucket():
 @pytest.mark.parametrize(
     'block_size', SIZES
 )
-def test_dummy(bucket, page_size, block_size):
+@pytest.mark.parametrize(
+    'journal_mode', JOURNAL_MODES
+)
+def test_dummy(bucket, page_size, block_size, journal_mode):
     s3vfs = S3VFS(bucket=bucket, block_size=block_size)
 
     with apsw.Connection("/a-test/cool.db", vfs=s3vfs.name) as db:
         cursor = db.cursor()
         cursor.execute(f'''
+            PRAGMA journal_mode = {journal_mode};
+        ''')
+        cursor.execute(f'''
             PRAGMA page_size = {page_size};
+        ''');
+        cursor.execute(f'''
             CREATE TABLE foo(x,y);
             INSERT INTO foo VALUES(1,2);
         ''')
