ece91a9ab9863d1c16d6835966b36063ee683e7b
Michal Charemza <michal@charemza.name>
2021-09-30 07:19:02 +0100

    refactor: remove whitespace

--- sqlite_s3vfs.py
+++ sqlite_s3vfs.py
@@ -12,7 +12,7 @@ class S3VFS(apsw.VFS):
         self.s3 = s3
         self.bucket = bucket
         apsw.VFS.__init__(self, self.vfsname, self.basevfs)
-        
+
     def xAccess(self, pathname, flags):
         if flags == apsw.mapping_access["SQLITE_ACCESS_EXISTS"]:
             return any(self.bucket.objects.filter(Prefix=pathname))
@@ -22,13 +22,13 @@ class S3VFS(apsw.VFS):
         elif flags == apsw.mapping_access["SQLITE_ACCESS_READ"]:
             # something something ACLs
             return True
-        
+
     def xDelete(self, filename, syncdir):
         self.bucket.objects.filter(Prefix=filename).delete()
 
     def xOpen(self, name, flags):
         return S3VFSFile(self.basevfs, name, flags, self.s3, self.bucket)
-    
+
 
 class S3VFSFile:
     def __init__(self, inheritfromvfsname, name, flags, s3, bucket):
@@ -38,7 +38,7 @@ class S3VFSFile:
             self.key = name
         self.s3 = s3
         self.bucket = bucket
-        
+
     def blocks(self, offset, amount):
         while amount > 0:
             block = offset // BLOCK_SIZE  # which block to get
@@ -47,10 +47,10 @@ class S3VFSFile:
             yield (block, start, consume)
             amount -= consume
             offset += consume
-            
+
     def block_object(self, block):
         return self.s3.Object(self.bucket.name, self.key + "/" + str(block))
-            
+
     def block(self, block):
         try:
             data = self.block_object(block).get()["Body"].read()
@@ -60,43 +60,43 @@ class S3VFSFile:
         assert type(data) is bytes
         assert len(data) == BLOCK_SIZE
         return data
-    
+
     def read(self, amount, offset):
         for block, start, consume in self.blocks(offset, amount):
             data = self.block(block)
             yield data[start:start+consume]
-            
+
     def xRead(self, amount, offset):
         return b"".join(self.read(amount, offset))
 
     def xFileControl(self, *args):
         return False
-    
+
     def xCheckReservedLock(self):
         return False
-    
+
     def xLock(self, level):
         pass
-        
+
     def xUnlock(self, level):
         pass
-    
+
     def xClose(self):
         pass
-    
+
     def xFileSize(self):
         return sum(o.size for o in self.bucket.objects.filter(Prefix=self.key + "/"))
-    
+
     def xSync(self, flags):
         return True
-    
+
     def xTruncate(self, newsize):
         return True
-    
+
     def xWrite(self, data, offset):
         for block, start, write in self.blocks(offset, len(data)):
             assert write <= len(data)
-            
+
             full_data = self.block(block)
             new_data = b"".join([
                 full_data[0:start],
