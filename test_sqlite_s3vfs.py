b8355e7273d7d305d9463254c4897b18d1e19780
Michal Charemza <michal@charemza.name>
2021-09-30 08:56:17 +0100

    tests: refactor to use a fixture

--- test_sqlite_s3vfs.py
+++ test_sqlite_s3vfs.py
@@ -1,10 +1,12 @@
 import apsw
 import boto3
+import pytest
 
 from sqlite_s3vfs import S3VFS
 
 
-def test_dummy():
+@pytest.fixture
+def bucket():
     session = boto3.Session(
         aws_access_key_id='AKIAIDIDIDIDIDIDIDID',
         aws_secret_access_key='aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
@@ -14,10 +16,13 @@ def test_dummy():
         endpoint_url='http://localhost:9000/'
     )
     bucket = s3.create_bucket(Bucket='my-bucket')
+    yield bucket
+    bucket.objects.all().delete()
+    bucket.delete()
+
+
+def test_dummy(bucket):
     s3vfs = S3VFS(bucket=bucket)
 
     db=apsw.Connection("/a-test/cool.db", vfs=s3vfs.vfsname)
     db.cursor().execute("create table foo(x,y); insert into foo values(1,2)")
-
-    bucket.objects.all().delete()
-    bucket.delete()
